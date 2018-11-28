3377d838f0d7fef1907586848a019acf6606240f
heosanghun <hsh0367@naver.com>
2018-11-28 15:31:15 +0900

    redotpage: change block url

--- redotpage/urls.py
+++ redotpage/urls.py
@@ -5,28 +5,32 @@ from .import views
 
 
 urlpatterns = [
+    #홈페이지 리뉴얼 작업 완료전까지 url 수정
 
-    url(r'^business/$', views.main_business, name='business'),
-    url(r'^pr/$', views.pr, name='pr'),
-    url(r'^$', views.main, name='main'),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^download/$', views.download, name='download'),
-    url(r'^signin/$', views.signIn, name='signin'),
-    url(r'^error/login/$', views.require, name='require'),
-    url(r'^error/user/$', views.user_error, name='errorUser'),
-    url(r'^board/(?P<pk>\d+)/check/$', views.board_user_check, name='board_user_check'),
-    url(r'^board/$', views.board, name='board'),
-    url(r'^board/view/(?P<pk>\d+)/$', views.board_view, name='board_view'),
-    url(r'^board/new/$', views.board_new, name='board_new'),
-    url(r'^board/(?P<pk>\d+)/edit/$', views.board_edit, name = 'board_edit'),
-    url(r'^board/(?P<pk>\d+)/delete/$', views.board_delete, name = 'board_delete'),
+    #리뉴얼전까지 막아두기
+    url(r'^$', views.block, name='block'),
+    url(r'test/business/$', views.main_business, name='business'),
+    url(r'test/pr/$', views.pr, name='pr'),
+    url(r'test/$', views.main, name='main'),
+    url(r'test/contact/$', views.contact, name='contact'),
+    url(r'test/download/$', views.download, name='download'),
+    url(r'test/signin/$', views.signIn, name='signin'),
+    url(r'test/error/login/$', views.require, name='require'),
+    url(r'test/error/user/$', views.user_error, name='errorUser'),
+    url(r'test/board/(?P<pk>\d+)/check/$', views.board_user_check, name='board_user_check'),
+    url(r'test/board/$', views.board, name='board'),
+    url(r'test/board/view/(?P<pk>\d+)/$', views.board_view, name='board_view'),
+    url(r'test/board/new/$', views.board_new, name='board_new'),
+    url(r'test/board/(?P<pk>\d+)/edit/$', views.board_edit, name='board_edit'),
+    url(r'test/board/(?P<pk>\d+)/delete/$', views.board_delete, name='board_delete'),
     # 이메일인증 및 리갭챠 구현전까지 회원가입 막음
     # 이메일 인증 회원가입 테스트
-    url( r'^register/$', views.signup, name='register'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    url(r'test/register/$', views.signup, name='register'),
+    url(r'test/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
 
-    url(r'^signout/$', logout, {'next_page': 'main'}, name='logout'),
-    #url(r'^testdownload/$', views.testdownload, name='testdownload'),
+    url(r'test/signout/$', logout, {'next_page': 'main'}, name='logout'),
+    # url(r'^testdownload/$', views.testdownload, name='testdownload'),
+
 
 ]
